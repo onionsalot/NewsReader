d5573af30837cd47ddf649f41f94dd0690977d98
Trong Nguyen <onionsinmypants@gmail.com>
2018-09-22 09:44:04 -0400

    Final rendition of navigation backstacks. Everything SHOULD be working

--- app/src/main/java/org/example/trongnguyen/newsreader/MainActivity.java
+++ app/src/main/java/org/example/trongnguyen/newsreader/MainActivity.java
@@ -1,6 +1,8 @@
 package org.example.trongnguyen.newsreader;
 
+
 import android.app.LoaderManager;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.content.SharedPreferences;
@@ -8,9 +10,12 @@ import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -31,10 +36,13 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawer;
     MenuItem searchItem;
     int fragmentSelected; // 0 = Main; 1 = Settings; 2 = Favorites
+    Fragment mainFragment;
+    Fragment settingsFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SharedPreferences themePref = PreferenceManager.getDefaultSharedPreferences(this);
+        final SharedPreferences themePref = PreferenceManager.getDefaultSharedPreferences(this);
         themeChooser(themePref.getString("theme", "1"));
         super.onCreate(savedInstanceState);
         // For first time runs, the preferences will not load quick enough and cause the app
@@ -64,6 +72,12 @@ public class MainActivity extends AppCompatActivity {
         drawer.addDrawerListener(toggle);
 
 
+        /*
+         * Instantiate the fragments
+         */
+        mainFragment = new MainFragment();
+        settingsFragment = new SettingsFragment();
+
         /* SavedInstanceState usually only is not null if the activity had never been fired up.
          * Cold loading would result in the SIS being null, which would then call for the
          * MainFragment to be loaded. If the activity is destroyed but not cold loaded, the MainFragment
@@ -71,8 +85,9 @@ public class MainActivity extends AppCompatActivity {
          */
         if (savedInstanceState == null) {
             Log.d(TAG, "onCreate: reloading the main fragment due to savedInstanceState being null");
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new MainFragment(), "main").commit();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_container, mainFragment, "main");
+            transaction.commit();
         }
         toggle.syncState();
 
@@ -85,56 +100,35 @@ public class MainActivity extends AppCompatActivity {
          * This will house the choices and functions for each of the Navigation objects that are clickable.
          * Each navigation will function in a similar way.
          *
-         * Check to see if either of the fragments NOT picked are active. If they are, hide them.
-         * Finally check if the fragment picked had ever been initialized. If it has, simply "show"
-         * it into frontal view. If it has not, "add" it to the current frontal View. Add is used because
-         * it will not destroy the fragment that it is replacing. "replace" is another option but that would
-         * destroy the replaced fragment unless backstacks are used. The way that the app functions now,
-         * there is no real need for backstacks are "searches" and "details" are both performed on exterior
-         * Intents.
-         *
-         * REVISED*
+         * Checks which fragment is currently active. If user clicks on the same fragment again, the shade
+         * will close and do nothing. If a new fragment is clicked, either pop it off the backstack if
+         * it is the main fragment or set it to the new one if it is settings.
          *
          */
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment settingsTest = (Fragment) getSupportFragmentManager().findFragmentByTag("settings");
                 switch (item.getItemId()) {
                     case R.id.nav_home:
-                        switch (fragmentSelected) {
-                            case 0: // Currently active fragment : Main
-                                // Do nothing. Already selected
-                                break;
-                            case 1: // Currently active fragment : Settings
-                                getSupportFragmentManager().popBackStackImmediate();
-                                break;
-                            case 2: // Currently active fragment : Favorites
-                                getSupportFragmentManager().popBackStackImmediate();
-                                break;
-                            default:
-                                break;
+                        if (settingsTest != null && settingsTest.isVisible()) {
+                            getSupportFragmentManager().popBackStackImmediate();
                         }
-                        fragmentSelected = 0;
+                        else {
+                            // Do nothing. Main is visible
+                        }
                         searchItem.setVisible(true);
                         break;
                     case R.id.nav_settings:
-                        switch (fragmentSelected) {
-                            case 0: // Currently active fragment : Main
-                                getSupportFragmentManager().beginTransaction()
-                                        .addToBackStack("main").replace(R.id.fragment_container,
-                                        new SettingsFragment(), "settings").commit();
-                                break;
-                            case 1: // Currently active fragment : Settings
-                                // Do nothing. Already selected.
-                                break;
-                            case 2: // Currently active fragment : Favorites
-                                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                                        new SettingsFragment(), "settings").commit();
-                                break;
-                            default:
-                                break;
+                        if (settingsTest != null && settingsTest.isVisible()) {
+                            // Do nothing, Settings is visible
                         }
-                        fragmentSelected = 1;
+                        else {
+                            transaction.addToBackStack("main");
+                            transaction.replace(R.id.fragment_container, settingsFragment, "settings");
+                            transaction.commit();
+                        }
                         searchItem.setVisible(false);
                         break;
                     case R.id.nav_favorites:
@@ -158,23 +152,40 @@ public class MainActivity extends AppCompatActivity {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            switch (fragmentSelected) {
-                case 0: // Currently active fragment : Main
-                    //TODO: Main selected already. Create user promp
-                    break;
-                case 1: // Currently active fragment : Settings
-                    getSupportFragmentManager().beginTransaction().hide(getSupportFragmentManager().findFragmentByTag("settings")).commit();
-                    searchItem.setVisible(true);
-                    break;
-                case 2: // Currently active fragment : Favorites
-                    getSupportFragmentManager().beginTransaction().hide(getSupportFragmentManager().findFragmentByTag("favorites")).commit();
-                    searchItem.setVisible(true);
-                    break;
-                default:
-                    break;
+            Fragment main = (Fragment) getSupportFragmentManager().findFragmentByTag("main");
+            if (main != null && main.isVisible()) {
+                // Main fragment is visible; Prompt user if they want to close.
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+                builder1.setMessage("Exit News Reader?");
+                builder1.setCancelable(true);
+
+
+                builder1.setPositiveButton(
+                        "Yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                                MainActivity.super.onBackPressed();
+                            }
+                        });
+
+                builder1.setNegativeButton(
+                        "No",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
             }
-            fragmentSelected = 0;
-            super.onBackPressed();
+            else {
+                // Settings is visible; hide and open the main fragment again.
+                getSupportFragmentManager().beginTransaction().hide(getSupportFragmentManager().findFragmentByTag("settings")).commit();
+                searchItem.setVisible(true);
+                super.onBackPressed();
+            }
         }
     }
 
@@ -182,6 +193,13 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         searchItem = menu.findItem(R.id.action_menu_search);
+        Fragment test = (Fragment) getSupportFragmentManager().findFragmentByTag("settings");
+        if (test != null && test.isVisible()) {
+            searchItem.setVisible(false);
+        }
+        else {
+            searchItem.setVisible(true);
+        }
         return true;
     }
 
@@ -194,7 +212,6 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
                 startActivity(intent);
                 return true;
-
         }
         return super.onOptionsItemSelected(item);
     }
