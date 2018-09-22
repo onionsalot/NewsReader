6706c1342835e4c71b05ab6a5e2bd9e46b68add1
Trong Nguyen <onionsinmypants@gmail.com>
2018-09-22 12:55:01 -0400

    Forgot to theme the about page oops

--- app/src/main/java/org/example/trongnguyen/newsreader/AboutActivity.java
+++ app/src/main/java/org/example/trongnguyen/newsreader/AboutActivity.java
@@ -1,5 +1,7 @@
 package org.example.trongnguyen.newsreader;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,7 +9,32 @@ public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final SharedPreferences themePref = PreferenceManager.getDefaultSharedPreferences(this);
+        themeChooser(themePref.getString("theme", "1"));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+    public void themeChooser(String theme) {
+        switch (theme) {
+            case "1":
+                setTheme(R.style.AppTheme);
+                break;
+            case "2":
+                setTheme(R.style.Midnight);
+                break;
+            case "3":
+                setTheme(R.style.cottonCandy);
+                break;
+            case "4":
+                setTheme(R.style.rockRoses);
+                break;
+            case "5":
+                setTheme(R.style.limeContrast);
+                break;
+            case "6":
+                setTheme(R.style.moodyRain);
+                break;
+
+        }
     }
 }
